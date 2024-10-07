69430bf30d7cd32948b26197a1a289e555938729
Joseph-Meiste <jomeeb2@student.oaisd.org>
2024-10-07 13:01:50 +0000

    Results

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("My name is Joseph Meiste");
+
+        //Using Variables
+        int age = 17;
+        string greeting = "Hello World";
+        string firstname = "Joseph";
+        string lastname = "Meiste";
+        double money = 2.79;
+
+        Console.WriteLine(greeting + "! " + "My name is " + firstname + " " + lastname + ". I am " + age + " years old. I just spent $" + money + " on an Energy Drink.");
+    }
+}
